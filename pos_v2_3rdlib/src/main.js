98e2134ac50721f8a8f6614c2bf502f8252a7bb9
zengpuyang <zengpuyang@jimitec.com>
2014-11-08 17:44:22 +0800

    7

--- pos_v2_3rdlib/src/main.js
+++ pos_v2_3rdlib/src/main.js
@@ -1,1 +1,40 @@
 //TODO: Please write code in this file.
+function printInventory(inputs) {
+    var zongji=0,shuchu='',free='',jiesheng= 0;
+    for(var i=0;i<inputs.length;i++){
+        inputs[i]+='-1';
+        var arr=inputs[i].split('-',2),count=1,c=0;
+        var h=_.findLastIndex(loadAllItems(), {
+            'barcode': arr[0]
+        });
+        zongji+=(loadAllItems()[h]).price*arr[1];
+        if((_.first(inputs,i)).indexOf(inputs[i])!=-1){continue}
+        for(var j=i+1;j<inputs.length;j++){
+            if(arr[0]==inputs[j]){
+                count+=1
+            }
+        }
+        count+=arr[1]-1;
+        var sjcount=count;
+        if(count>=3 && loadPromotions.call()[0].barcodes.indexOf(arr[0])!=-1 ){
+            c=parseInt(count/3);zongji-=c*loadAllItems()[h].price;sjcount=count-c;
+            free+='名称：'+loadAllItems()[h].name+'，数量：'+c+loadAllItems()[h].unit+'\n';
+            jiesheng+=c*loadAllItems()[h].price}
+        shuchu+='名称：'+loadAllItems()[h].name+'，数量：'+count+loadAllItems()[h].unit+
+            '，单价：'+loadAllItems()[h].price.toFixed(2)+'(元)，小计：'+(loadAllItems()[h].price*sjcount).toFixed(2)+'(元)\n';
+    }
+    var date = function (num) {
+        return num < 10 ? '0' + num : num;
+    };
+    console.log('***<没钱赚商店>购物清单***\n'+
+        '打印时间：'+moment().format('YYYY[年]MM[月]DD[日] HH:mm:ss')+'\n'+
+        '----------------------\n' +
+        shuchu+
+        '----------------------\n'+
+        '挥泪赠送商品：\n'+
+        free+
+        '----------------------\n'+
+        '总计：'+zongji.toFixed(2)+'(元)\n'+
+        '节省：'+jiesheng.toFixed(2)+'(元)\n'+
+        '**********************')
+}
