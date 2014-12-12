34eee466e7c7e04f5b3503c9f4b78cf98e609fdf
zengpuyang <zengpuyang@jimitec.com>
2014-12-12 15:40:04 +0800

    pos_v0.2 rename

--- pos_v0.2/src/item_way.js
+++ pos_v0.2/src/item_way.js
@@ -1,12 +1,12 @@
 function item_add_min_pay(inputs){
-    var input=_.toArray(_.groupBy(inputs, function (code) {
+    var input_group=_.toArray(_.groupBy(inputs, function (code) {
         return code
     }));
     var new_inputs=[],index=0;
-    _.each(input, function (code) {
+    _.each(input_group, function (code_array) {
         _.each(loadAllItems(),function(codes){
-                if(codes.barcode==code[0]){
-                    codes.count=code.length;
+                if(codes.barcode==code_array[0]){
+                    codes.count=code_array.length;
                     codes.min_pay = (codes.count * codes.price);
                     new_inputs[index]=codes;
                     index+=1
@@ -15,9 +15,9 @@ function item_add_min_pay(inputs){
     });
     return new_inputs
 }
-function item_receipt(new_inputs){
+function item_receipt(inputs_with_method){
     var total_money= 0,receipt='';
-    _.each(new_inputs,function(item){
+    _.each(inputs_with_method,function(item){
         receipt+= '名称：'+item.name+'，数量：'+item.count+item.unit+
             '，单价：'+item.price.toFixed(2)+'(元)，小计：'+item.min_pay.toFixed(2)+'(元)\n';
         total_money+=item.min_pay
--- pos_v0.2/src/main.js
+++ pos_v0.2/src/main.js
@@ -1,6 +1,6 @@
 function printInventory(inputs) {
-    var new_inputs=item_add_min_pay(inputs);
-    var receipt_list=item_receipt(new_inputs);
+    var inputs_with_method=item_add_min_pay(inputs);
+    var receipt_list=item_receipt(inputs_with_method);
     var result='***<没钱赚商店>购物清单***\n'+
         receipt_list.receipt+
         '----------------------\n' +
