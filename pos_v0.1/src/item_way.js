dbdf6074f850be014bdc1fd3a962f375ab753af5
zengpuyang <zengpuyang@jimitec.com>
2014-12-12 21:12:47 +0800

    pos_v0.1 rename

--- pos_v0.1/src/item_way.js
+++ pos_v0.1/src/item_way.js
@@ -1,20 +1,20 @@
-function item_add_min_pay(inputs){
+function item_method_subtotal(inputs){
     return _.map(_.toArray(_.groupBy(inputs, function (code) {
         return code.barcode
     })), function (code) {
-        code[0].min_pay = (code.length * code[0].price);
+        code[0].subtotal = (code.length * code[0].price);
         code[0].count=code.length;
         return code[0]
     });
 }
-function item_receipt(new_inputs){
-    var total_money= 0,receipt='';
-    _.each(new_inputs,function(item){
+function item_receipt(curt_inputs){
+    var total= 0,receipt='';
+    _.each(curt_inputs,function(item){
         receipt+= '名称：'+item.name+'，数量：'+item.count+item.unit+
-            '，单价：'+item.price.toFixed(2)+'(元)，小计：'+item.min_pay.toFixed(2)+'(元)\n';
-        total_money+=item.min_pay
+            '，单价：'+item.price.toFixed(2)+'(元)，小计：'+item.subtotal.toFixed(2)+'(元)\n';
+        total+=item.subtotal
     });
     return{
     receipt:receipt,
-    total_money:total_money.toFixed(2)}
+    total:total.toFixed(2)}
 }
