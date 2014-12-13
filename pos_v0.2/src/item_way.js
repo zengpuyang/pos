cdfbe86110fbca3159b3149ed7b4efd0afeb567b
zengpuyang <zengpuyang@jimitec.com>
2014-12-13 17:27:47 +0800

    pos_v0.2 adjust

--- pos_v0.2/src/item_way.js
+++ pos_v0.2/src/item_way.js
@@ -1,28 +1,27 @@
-function item_add_min_pay(inputs){
-    var input_group=_.toArray(_.groupBy(inputs, function (code) {
-        return code
-    }));
-    var new_inputs=[],index=0;
-    _.each(input_group, function (code_array) {
-        _.each(loadAllItems(),function(codes){
-                if(codes.barcode==code_array[0]){
-                    codes.count=code_array.length;
-                    codes.min_pay = (codes.count * codes.price);
-                    new_inputs[index]=codes;
-                    index+=1
-                }
-        });
-    });
-    return new_inputs
+function item_method_subtotal(inputs){
+    return _.chain(inputs)
+        .groupBy()
+        .toArray()
+        .map(function (code_array) {
+            var count = code_array.length;
+            _.chain(loadAllItems())
+                .filter(function(codes){return codes.barcode==code_array[0]})
+                .map(function(codes){
+                    codes.count = count;
+                    codes.subtotal = (codes.count * codes.price);
+                    code_array[0] = codes
+            });
+            return code_array[0]
+        }).value();
 }
 function item_receipt(inputs_with_method){
-    var total_money= 0,receipt='';
+    var total= 0,receipt='';
     _.each(inputs_with_method,function(item){
-        receipt+= '名称：'+item.name+'，数量：'+item.count+item.unit+
-            '，单价：'+item.price.toFixed(2)+'(元)，小计：'+item.min_pay.toFixed(2)+'(元)\n';
-        total_money+=item.min_pay
+        receipt += '名称：'+item.name+'，数量：'+item.count+item.unit+
+            '，单价：'+item.price.toFixed(2)+'(元)，小计：'+item.subtotal.toFixed(2)+'(元)\n';
+        total += item.subtotal
     });
     return {
         receipt: receipt,
-        total_money:total_money.toFixed(2)}
+        total:total.toFixed(2)}
 }
--- pos_v0.2/src/main.js
+++ pos_v0.2/src/main.js
@@ -1,10 +1,10 @@
 function printInventory(inputs) {
-    var inputs_with_method=item_add_min_pay(inputs);
+    var inputs_with_method=item_method_subtotal(inputs);
     var receipt_list=item_receipt(inputs_with_method);
     var result='***<没钱赚商店>购物清单***\n'+
         receipt_list.receipt+
         '----------------------\n' +
-        '总计：'+receipt_list.total_money+'(元)\n' +
+        '总计：'+receipt_list.total+'(元)\n' +
         '**********************';
     console.log(result)
 }
