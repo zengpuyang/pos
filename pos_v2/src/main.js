20275bcffd41d12c2608cf741291516480a2744d
zengpuyang <zengpuyang@jimitec.com>
2014-12-22 19:19:53 +0800

    pos_v2 the time adjust

--- pos_v2/src/main.js
+++ pos_v2/src/main.js
@@ -1,53 +1,17 @@
-//TODO: Please write code in this file.
 function printInventory(inputs) {
-    var zongji=0,shuchu='',free='',jiesheng= 0;
-    for(var i=0;i<inputs.length;i++){
-        inputs[i]+='-1';
-        var arr=inputs[i].split('-',2);
-        for(var h=0;h<loadAllItems().length;h++){
-            if(loadAllItems()[h].barcode==arr[0]){
-                break
-            }
-        }
-        zongji+=(loadAllItems()[h]).price*arr[1];
-        var count=1,c=0;
-        for(var k=0;k<i;k++){
-            if(inputs[i]==inputs[k]){
-                c=1
-            }
-        }
-        if(c==1){continue}
-        for(var j=i+1;j<inputs.length;j++){
-            if(arr[0]==inputs[j]){
-                count+=1
-            }
-        }
-        count+=arr[1]-1;
-        var sjcount=count;
-        if(count>=3 && loadPromotions.call()[0].barcodes.indexOf(arr[0])!=-1 ){
-            c=parseInt(count/3);zongji-=c*loadAllItems()[h].price;sjcount=count-c;
-            free+='名称：'+loadAllItems()[h].name+'，数量：'+c+loadAllItems()[h].unit+'\n';
-            jiesheng+=c*loadAllItems()[h].price}
-        shuchu+='名称：'+loadAllItems()[h].name+'，数量：'+count+loadAllItems()[h].unit+
-            '，单价：'+loadAllItems()[h].price.toFixed(2)+'(元)，小计：'+(loadAllItems()[h].price*sjcount).toFixed(2)+'(元)\n';
-    }
-    var date = function (num) {
-        return num < 10 ? '0' + num : num;
-    };
-    console.log('***<没钱赚商店>购物清单***\n'+
-        '打印时间：'+date((new Date).getFullYear())+ '年' +
-        date((new Date).getMonth() + 1)+ '月' +
-        date((new Date).getDate()) + '日 ' +
-        date((new Date).getHours())+ ':' +
-        date((new Date).getMinutes())+ ':' +
-        date((new Date).getSeconds())+'\n'+
+    var inputs_with_method=item_handled(inputs);
+    var receipt_list=item_receipt_lists(inputs_with_method);
+    var time=moment().format('YYYY[年]MM[月]DD[日] HH:mm:ss');
+    var result='***<没钱赚商店>购物清单***\n'+
+        '打印时间：'+time+'\n'+
         '----------------------\n' +
-        shuchu+
-        '----------------------\n'+
-        '挥泪赠送商品：\n'+
-        free+
-        '----------------------\n'+
-        '总计：'+zongji.toFixed(2)+'(元)\n'+
-        '节省：'+jiesheng.toFixed(2)+'(元)\n'+
-        '**********************')
+        receipt_list.receipt_all+
+        '----------------------\n' +
+        '挥泪赠送商品：\n' +
+        receipt_list.receipt_free+
+        '----------------------\n' +
+        '总计：'+receipt_list.total+'(元)\n' +
+        '节省：'+receipt_list.save +'(元)\n' +
+        '**********************';
+    console.log(result)
 }
--- pos_v2/src/model/item.js
+++ pos_v2/src/model/item.js
@@ -4,3 +4,45 @@ function Item(barcode, name, unit, price) {
     this.unit = unit;
     this.price = price || 0.00;
 }
+function item_handled(inputs){
+    return _.chain(inputs)
+        .map(function(code){return code+'-1'})
+        .groupBy(function(code){return code.substr(0,10)})
+        .toArray()
+        .map(function(codes){
+            return {
+                barcode:codes[0].split('-',2)[0],
+                count: _.reduce(codes,function(code1,code2){
+                    return code1+parseInt(code2.split('-',2)[1])
+                },0)
+            }
+        })
+        .each(function(code){
+            var code_method=_.filter(loadAllItems(),function(item){return item.barcode==code.barcode});
+            code.name=code_method[0].name;
+            code.unit=code_method[0].unit;
+            code.price=code_method[0].price;
+            code.subtotal=code_method[0].price*code.count
+        })
+        .value()
+}
+function item_receipt_lists(inputs_with_method){
+    var total= 0,save= 0,receipt_all='',receipt_free='';
+    _.each(inputs_with_method,function(item){
+        if(item.count>2 && loadPromotions()[0].barcodes.indexOf(item.barcode)!=-1){
+            var save_count=parseInt(item.count/3);
+            receipt_free+='名称：'+item.name+'，数量：'+save_count+item.unit+'\n';
+            item.subtotal-=save_count*item.price;
+            save+=save_count*item.price
+        }
+        receipt_all += '名称：'+item.name+'，数量：'+item.count+item.unit+
+            '，单价：'+item.price.toFixed(2)+'(元)，小计：'+item.subtotal.toFixed(2)+'(元)\n';
+        total += item.subtotal
+    });
+    return {
+        receipt_all: receipt_all,
+        receipt_free:receipt_free,
+        total:total.toFixed(2),
+        save:save.toFixed(2)
+    }
+}
